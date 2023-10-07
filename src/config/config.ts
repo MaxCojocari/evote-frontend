191c7b20f4fd68a68cac4a5cf71c0433251603eb
MaxCojocari <cojocarigoncearmax@gmail.com>
2023-10-07 17:17:47 +0300

    feat: auth service

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = "http://localhost:8000";
 export const APP_URL = "http://localhost:3000";
-// export const APP_URL = "https://swiftvote.md";
+export const JWT_SECRET = "M3ljN0IzPmgvaWhrZGRDXCpYMzkjWC0=";
